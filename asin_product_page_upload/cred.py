9513a80b032b4df382f23ab92381aa3dd3118b08
venkat-thrymr <89445223+venkat-thrymr@users.noreply.github.com>
2021-08-31 16:44:56 +0530

    Add files via upload

---
+++ asin_product_page_upload/cred.py
@@ -0,0 +1,7 @@
+data = {
+    "host"      : "test-wrapfy-1.c9dyjmixlq3h.us-east-2.rds.amazonaws.com",
+    "database"  : "wrapfy_test",
+    "user"      : "test_postgre_1",
+    "password"  : "wrapfytesting",
+    "port"      : "5432"
+}
